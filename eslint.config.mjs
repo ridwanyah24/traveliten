875e1bfd5db7aacad6957355e28d272fe47bbe7f
ridwanyah24 <ridwangames24@gmail.com>
2025-01-24 00:42:49 +0100

    latest build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,12 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
